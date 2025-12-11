cca168a12e36bbf550ba7955e60c92437c8ff9e7
stev <stephin.softbuilders@gmail.com>
2025-12-11 09:56:19 +0400

    update

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,10 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: "/fisherman-basket-website/",
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
