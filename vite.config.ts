b3e2a7c297f05394b92c2f23b4dd5fab06df27c1
stev <stephin.softbuilders@gmail.com>
2025-12-11 15:43:18 +0400

    update

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: "/fisherman-basket-website/",
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean
   ),
